a3809ee13c6b31eaca767c0aa19fc1b365483d8d
Renan Storrer <storrerrenan@gmail.com>
2022-06-28 15:08:29 -0300

    exercicio DOM

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1--javascript-dom-seletores/script.js
@@ -0,0 +1,6 @@
+document.getElementById('page-title').innerText = 'Ilha do medo'
+document.getElementById('subtitle').innerText = 'Ator principal:'
+document.getElementById('paragraph').innerText = 'Na década de 50, a fuga de uma assassina leva o detetive Teddy Daniels e seu parceiro a investigarem o seu desaparecimento de um quarto trancado em um hospital psiquiátrico. Lá, uma rebelião se inicia e o agente terá que enfrentar seus próprios medos.'
+document.getElementById('second-paragraph').innerText = 'Leonardo Di Caprio'
+let paragraph = document.getElementById('paragraph')
+paragraph.style.color = 'red'
