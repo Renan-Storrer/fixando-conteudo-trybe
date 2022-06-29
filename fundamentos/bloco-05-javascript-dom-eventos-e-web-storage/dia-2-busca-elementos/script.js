4f80ccb51b5d945916ba78f8eac36c7f868b2f8b
Renan Storrer <storrerrenan@gmail.com>
2022-06-29 15:19:45 -0300

    exercicio

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-busca-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-busca-elementos/script.js
@@ -15,3 +15,24 @@ let textElement = elementoOndeVoceEsta.nextSibling;
 let terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
 
 let terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+
+let pai2 = document.getElementById('pai');
+let irmaoElementoOndeVoceEsta = document.createElemente('section');
+irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta'
+pai.appendChild(irmaoElementoOndeVoceEsta);
+
+let elementoOndeVoceEsta2 = document.getElementById('elementoOndeVoceEsta');
+let filhoElementoOndeVoceEsta = document.createElement('section');
+filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+let primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho')
+let filhoPrimeiroFilhoDoFilho2 = document.createElement('section');
+filhoPrimeiroFilhoDoFilho2.id = 'filhoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho2);
+
+let terceiroFilho3 = filhoPrimeiroFilhoDoFilho2
+    .parentElement // primeiroFilhoDoFilho
+    .parentElement // elementoOndeVoceEsta
+    .nextElementSibling; // terceiroFilho
+console.log(terceiroFilho);
