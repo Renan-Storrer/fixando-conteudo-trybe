e576eea71cccc27aa06a34e866d492d8e191f5f8
Renan Storrer <storrerrenan@gmail.com>
2022-06-29 15:00:20 -0300

    exercicio 1

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-busca-elementos/script.js
@@ -0,0 +1,5 @@
+let alteraCor = document.getElementById('elementoOndeVoceEsta').parentNode
+alteraCor.style.color = 'red'
+
+let adicionaTexto = document.getElementById('primeiroFilhoDoFilho')
+adicionaTexto.innerText = 'texto texto texto texto texto texto'
