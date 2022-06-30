a9b22f241f6ba1fb13d665527ec4f7f184b2fff7
Renan Storrer <storrerrenan@gmail.com>
2022-06-30 15:36:26 -0300

    exercicio 3 e exercicio 4

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-eventos/main.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-eventos/main.js
@@ -26,9 +26,18 @@ thirdLi.addEventListener('click', addClassTech)
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+input.addEventListener('input', function(evento) {
+    let calssTech = document.querySelector('.tech');
+    calssTech.innerText = evento.target.value
+});
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+
+myWebpage.addEventListener('dblclick', function() {
+    window.location.replace('https://renan-storrer.github.io/');
+  });
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
