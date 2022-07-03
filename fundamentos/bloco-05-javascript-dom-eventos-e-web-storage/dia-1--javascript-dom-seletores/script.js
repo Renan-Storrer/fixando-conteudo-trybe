dac99e7e2d1f7669b67acbd8aa771919d395022b
Renan Storrer <storrerrenan@gmail.com>
2022-07-03 03:45:45 -0300

    exercicio1.1

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1--javascript-dom-seletores/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1--javascript-dom-seletores/script.js
@@ -1,11 +1,5 @@
-document.getElementById('page-title').innerText = 'Ilha do medo'
-document.getElementById('subtitle').innerText = 'Ator principal:'
-document.getElementById('paragraph').innerText = 'Na década de 50, a fuga de uma assassina leva o detetive Teddy Daniels e seu parceiro a investigarem o seu desaparecimento de um quarto trancado em um hospital psiquiátrico. Lá, uma rebelião se inicia e o agente terá que enfrentar seus próprios medos.'
-document.getElementById('second-paragraph').innerText = 'Leonardo Di Caprio'
-let paragraph = document.getElementById('paragraph')
-paragraph.style.color = 'red'
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
 
-// let parag = document.getElementsByClassName("parag");
-// for (let index in parag) {
-//    parag[index].innerText = 'parag parag parag parag'
-// }
+let titulo = document.getElementById('page-title')
+titulo.innerText = 'ILHA DO MEDO'
