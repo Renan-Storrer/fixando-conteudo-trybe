2c5bede7d65916b77f52a30c4df32bb3e9742c07
Renan Storrer <storrerrenan@gmail.com>
2022-07-03 03:50:32 -0300

    exercicio 1.2 e 1.3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1--javascript-dom-seletores/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1--javascript-dom-seletores/script.js
@@ -1,5 +1,11 @@
-const paragraph = document.getElementById("paragraph");
-paragraph.style.color = "red";
-
 let titulo = document.getElementById('page-title')
 titulo.innerText = 'ILHA DO MEDO'
+
+let segundoParagrafo = document.getElementById('second-paragraph')
+segundoParagrafo.innerText = 'filme sobre a loucura da sua mente e como ela interagem sobre o mundo externo'
+
+let subititulo = document.getElementById('subtitle')
+subititulo.innerText = 'Sobre:'
+
+let paragrafo = document.getElementById('paragraph')
+paragrafo.style.color = 'blue'
