d822f2cb2ee1abca6aaa638d3a6e16672a3b1c74
Renan Storrer <storrerrenan@gmail.com>
2022-07-03 03:55:27 -0300

    exercicio 2.1, 2.2

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1--javascript-dom-seletores/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1--javascript-dom-seletores/script.js
@@ -9,3 +9,13 @@ subititulo.innerText = 'Sobre:'
 
 let paragrafo = document.getElementById('paragraph')
 paragrafo.style.color = 'blue'
+
+paragrafo.className = 'style'
+segundoParagrafo.classList.add ('style')
+// console.log(paragrafo);
+// console.log(segundoParagrafo);
+
+let style1 = document.getElementsByClassName('style')[0];
+let style2 = document.getElementsByClassName('style')[1];
+// console.log(style1);
+// console.log(style2);
