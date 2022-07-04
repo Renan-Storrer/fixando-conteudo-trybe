f95b82faa9e40bc3eef4920f2dce3908e7fdd6c8
Renan Storrer <storrerrenan@gmail.com>
2022-07-04 02:13:48 -0300

    exercicio 1 e 2

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-busca-elementos/2/script.js
@@ -0,0 +1,7 @@
+let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+// console.log(elementoOndeVoceEsta);
+
+let paiEOVE = document.getElementById('elementoOndeVoceEsta').parentNode
+// console.log(paiEOVE);
+paiEOVE.style.color = 'red'
+
