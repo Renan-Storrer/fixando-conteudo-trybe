8378ebd2f4897e0b5356daeb13328017d2f00b44
Renan Storrer <storrerrenan@gmail.com>
2022-07-04 02:17:05 -0300

    exercicio 3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-busca-elementos/2/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-busca-elementos/2/script.js
@@ -5,3 +5,9 @@ let paiEOVE = document.getElementById('elementoOndeVoceEsta').parentNode
 // console.log(paiEOVE);
 paiEOVE.style.color = 'red'
 
+let primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+// console.log(primeiroFilhoDoFilho);
+let p = document.createElement('p')
+primeiroFilhoDoFilho.appendChild(p)
+p.innerText = "Olá, tudo bem ?"
+p.style.color = 'green'
