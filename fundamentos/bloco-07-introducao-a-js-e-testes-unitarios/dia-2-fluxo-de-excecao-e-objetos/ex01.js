aeb7464e55d3b195d560e17b9e03d285abf39ace
Renan Storrer <storrerrenan@gmail.com>
2022-07-15 15:51:14 -0300

    fixando

--- fundamentos/bloco-07-introducao-a-js-e-testes-unitarios/dia-2-fluxo-de-excecao-e-objetos/ex01.js
+++ fundamentos/bloco-07-introducao-a-js-e-testes-unitarios/dia-2-fluxo-de-excecao-e-objetos/ex01.js
@@ -1,7 +1,7 @@
 // const sum = (value1, value2) => value1 + value2;
 // console.log(sum(2, 3));
 
-////---------------------------//-----------------//---------
+// //---------------------------//-----------------//---------
 
 // const sum = (value1, value2) => {
 //   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
@@ -12,13 +12,32 @@
 
 // console.log(sum(2, '3'));
 
-////----------------------------//-----------------//--------
+// //----------------------------//-----------------//--------
 
-const sum = (value1, value2) => {
+// const sum = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     throw new Error('Os valores devem ser numéricos');
+//   }
+//   return value1 + value2;
+// };
+
+// console.log(sum(2, '3'));
+
+// //---------------------------//-----------------//---------
+
+const verifyIsNumber = (value1, value2) => {
   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
     throw new Error('Os valores devem ser numéricos');
   }
-  return value1 + value2;
+};
+
+const sum = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 console.log(sum(2, '3'));
